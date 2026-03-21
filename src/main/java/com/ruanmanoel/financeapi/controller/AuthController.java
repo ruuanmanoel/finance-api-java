357400c6ff8347fbb2d1f42435a98a30db22c786
Ruan Manoel <ruuanmanoel@gmail.com>
2026-03-20 23:33:52 -0300

    feat: add authentication and authorization with JWT

--- src/main/java/com/ruanmanoel/financeapi/controller/AuthController.java
+++ src/main/java/com/ruanmanoel/financeapi/controller/AuthController.java
@@ -1,27 +1,39 @@
-package com.ruanmanoel.financeapi.service;
+package com.ruanmanoel.financeapi.controller;
 
+import com.ruanmanoel.financeapi.config.TokenService;
 import com.ruanmanoel.financeapi.dto.LoginRequestDTO;
 import com.ruanmanoel.financeapi.dto.LoginResponseDTO;
+import com.ruanmanoel.financeapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/auth")
 public class AuthController {
     @Autowired
     UserService userService;
 
-    @PostMapping
+    @Autowired
+    TokenService tokenService;
+
+    @PostMapping("/login")
     public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
         boolean valido = userService.validarLogin(loginRequestDTO.getEmail(), loginRequestDTO.getSenha());
 
-        if(!valido) {
-            throw new RuntimeException("Email ou senha inválidos");
+        if (!valido) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED,
+                    "Email ou senha inválidos"
+            );
         }
 
-        return new LoginResponseDTO("Logado com sucesso");
+        String token = tokenService.gerarToken(loginRequestDTO.getEmail());
+
+        return new LoginResponseDTO(token);
     }
 }
